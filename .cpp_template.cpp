03dddf37f06b4a88118b614fedbed380d2dfedb5
MilosCicmanec <171834077+MilosCicmanec@users.noreply.github.com>
2026-07-14 17:58:03 +0200

    added template for codeforces (c++)

---
+++ .cpp_template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+using vi = vector<int>;
+using ll = long long;
+using vl = vector<long long>;
+
+void solve() {
+  ll mod = 1000000007;
+  int n;
+  cin >> n;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+  return 0;
+}
